f5d1404569ba5de46a27de013271285c3d1c4fb3
Monsterope <dachawat.fang57@sskru.ac.th>
2017-02-06 13:27:11 +0700

    Commit Lab 2

---
+++ ClassConvertCode/src/classconvertcode/ComputerPartDisplayVisitor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classconvertcode;
+
+/**
+ *
+ * @author MonsteropE
+ */
+public class ComputerPartDisplayVisitor{
+    
+    public void visit(Computer Computer){
+        System.out.print("kak");
+    }
+    
+    public void visit(Mouse Mouse){
+        
+    }
+    
+    public void visit(Keyboard Keyboard){
+        
+    }
+    
+    public void visit(Monitor Monitor){
+        
+    }
+}
